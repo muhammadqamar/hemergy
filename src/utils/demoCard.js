bb4c2ac0e50145ddd247d8c449562b78eff0f760
muhammadqamar <muhammadqamar111@gmail.com>
2023-02-16 14:57:40 +0500

    added new hemergy captha

--- src/utils/demoCard.js
+++ src/utils/demoCard.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
+
+import CalendlyModal from '@/utils/calendly';
 const DemoCard = ({ showIcon, heading, desc, btnText }) => {
+
   return (
     <div className="api-card-box">
       {showIcon && <Image src="/images/code.svg" alt="" width={51} height={51} />}
       <h1 className="api-heading">{heading}</h1>
       <p className="api-about">{desc}</p>
 
-      <Link href="" className="btn Primary">
-        {btnText}
-      </Link>
+      <CalendlyModal
+
+        btnText={btnText}
+        url={'https://calendly.com/ahmed-belabdia'}
+      />
     </div>
   );
 };
