314d4340542c14653fa089fd9a09822a0797141d
muhammadqamar111 <muhammadqamar111@gmail.com>
2023-02-21 01:04:56 +0500

    setup coisbase wallet connector

--- src/pages/on-boarding.js
+++ src/pages/on-boarding.js
@@ -2,22 +2,20 @@ import { useEffect, useState } from "react";
 
 import Image from "next/image";
 
-
 import RegisterSlider from "@/components/Authentications/registerSlider";
 import VerificationBox from "@/components/Authentications/verification";
 import InvestorProfile from "@/components/Authentications/investorProfile";
 import Financials from "@/components/Authentications/financials";
-import WalletOption from "@/utils/walletConnect/connectors";
+import AllWalletOption from "@/utils/walletConnect/allWalletOption";
 
 const Verification = () => {
-  const [step, setStep] = useState(1);
-  const [loader, setLoading] = useState(true)
-  const [userDetail, setUserDetail] = useState()
+  const [step, setStep] = useState(4);
+  const [loader, setLoading] = useState(true);
+  const [userDetail, setUserDetail] = useState();
 
-  useEffect(()=>{
-    setUserDetail({email: localStorage.getItem("hemergy-email")})
-  },[])
-
+  useEffect(() => {
+    setUserDetail({ email: localStorage.getItem("hemergy-email") });
+  }, []);
 
   return (
     <div className="authentications-section">
@@ -25,14 +23,16 @@ const Verification = () => {
         <Image src="/images/hemergy-logo.svg" width={150} height={32} alt="logo" />
       </div>
 
-      {loader && <div className="auth-container">
-        <RegisterSlider />
-        {step === 1 && <VerificationBox userDetail={userDetail} setStep={setStep} />}
-        {step === 2 && <InvestorProfile userDetail={userDetail} setStep={setStep} />}
-        {step === 3 && <Financials userDetail={userDetail} setStep={setStep} />}
-        {step === 4 && <WalletOption userDetail={userDetail} setStep={setStep} />}
-      </div>}
-
+      {loader && (
+        <div className="auth-container">
+          <RegisterSlider />
+          {step === 1 && <VerificationBox userDetail={userDetail} setStep={setStep} />}
+          {step === 2 && <InvestorProfile userDetail={userDetail} setStep={setStep} />}
+          {step === 3 && <Financials userDetail={userDetail} setStep={setStep} />}
+          {/* {step === 4 && <WalletOption userDetail={userDetail} setStep={setStep} />} */}
+          {step === 4 && <AllWalletOption userDetail={userDetail} setStep={setStep} />}
+        </div>
+      )}
 
       <div className="auth-wather" />
     </div>
@@ -40,5 +40,3 @@ const Verification = () => {
 };
 
 export default Verification;
-
-
--- src/utils/walletConnect/profile.js
+++ src/utils/walletConnect/profile.js
@@ -1,18 +1,11 @@
-import {
-  useAccount,
-  useConnect,
-  useDisconnect,
-  useEnsAvatar,
-  useEnsName,
-} from 'wagmi'
-
-export default function Profile() {
-  const { address, connector, isConnected } = useAccount()
-  const { data: ensAvatar } = useEnsAvatar({ address })
-  const { data: ensName } = useEnsName({ address })
-  const { connect, connectors, error, isLoading, pendingConnector } =
-    useConnect()
-  const { disconnect } = useDisconnect()
+import { useAccount, useConnect, useDisconnect, useEnsAvatar, useEnsName } from "wagmi";
+import Image from "next/image";
+export default function Profile({ setviewAllWallet, viewAllWallet }) {
+  const { address, connector, isConnected } = useAccount();
+  const { data: ensAvatar } = useEnsAvatar({ address });
+  const { data: ensName } = useEnsName({ address });
+  const { connect, connectors, error, isLoading, pendingConnector } = useConnect();
+  const { disconnect } = useDisconnect();
 
   if (isConnected) {
     return (
@@ -22,26 +15,63 @@ export default function Profile() {
         <div>Connected to {connector.name}</div>
         <button onClick={disconnect}>Disconnect</button>
       </div>
-    )
+    );
   }
 
   return (
     <div>
-      {connectors.map((connector) => (
+      {/* {connectors.map((connector) => (
         <button
           disabled={!connector.ready}
           key={connector.id}
           onClick={() => connect({ connector })}
         >
           {connector.name}
-          {!connector.ready && ' (unsupported)'}
-          {isLoading &&
-            connector.id === pendingConnector?.id &&
-            ' (connecting)'}
+          {!connector.ready && " (unsupported)"}
+          {isLoading && connector.id === pendingConnector?.id && " (connecting)"}
         </button>
-      ))}
+      ))} */}
 
-      {error && <div>{error.message}</div>}
+      {/* {error && <div>{error.message}</div>} */}
+
+      <div className="flex flex-wrap gap-5">
+        {connectors.map((connector) => {
+          var walletMask;
+          if (connector.id === "metaMask") {
+            walletMask = "/images/metamask.svg";
+          }
+          if (connector.id === "coinbaseWallet") {
+            walletMask = "/images/zerion.svg";
+          }
+          if (connector.id === "walletConnect") {
+            walletMask = "/images/ledger.svg";
+          }
+
+          console.log("conec", connector);
+          return (
+            <div
+              className="flex items-center gap-0.5  flex-col"
+              key={connector.id}
+              onClick={() => connect({ connector })}
+            >
+              <Image src={walletMask} alt="logo" width={64} height={64} />
+              <p className="p-sm-semi font-medium text-textblack">{connector.name}</p>
+            </div>
+          );
+        })}
+
+        {!viewAllWallet && (
+          <div
+            className="flex items-center gap-0.5 flex-col"
+            onClick={() => {
+              setviewAllWallet(true);
+            }}
+          >
+            <Image src="/images/allWallet.svg" alt="logo" width={64} height={64} />
+            <p className="p-sm-semi font-medium text-textblack">View all</p>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
