1f1d602aa66623148f2f4bebf6d2cd603e5bb491
muhammadqamar111 <muhammadqamar111@gmail.com>
2023-03-07 14:32:04 +0500

    fix signup issue

--- src/pages/register.js
+++ src/pages/register.js
@@ -11,6 +11,19 @@ const Register = ({ query }) => {
     console.log(query);
     if (query?.email) {
       setRegisterState({ email: query?.email });
+    } else {
+      if (query?.status === 400) {
+        toast.error("Email Already Exist", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
     }
   }, [query]);
 
